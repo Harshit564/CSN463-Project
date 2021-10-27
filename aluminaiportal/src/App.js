b37fb362e4ff717a9a25f13a2bfb1886474cd76e
Hp-11 <64689840+Hp-11@users.noreply.github.com>
2021-10-27 08:14:20 +0530

    completed mainfeed design using material ui

--- aluminaiportal/src/App.js
+++ aluminaiportal/src/App.js
@@ -1,12 +1,16 @@
-
 import './App.css';
+import Header from './assests/header';
+import Footer from './assests/footer';
 import MainFeed from './mainFeed/mainfeed';
+import ProfilePage from './profile/profilePage';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
       <MainFeed />
+      <ProfilePage />
+      <Footer />
     </div>
   );
 }
---
+++ aluminaiportal/src/profile/profilePage.js
@@ -0,0 +1,13 @@
+import './profilePage.css';
+
+function profilePage(){
+    return(
+        <div>
+            <div className="profilePage">
+                hi this is profile page
+            </div>
+        </div>
+    );
+}
+
+export default profilePage;
